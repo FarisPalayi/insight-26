4aedc06131d0ab7c01baa4919cb8bccbb549fb28
Faris P <farispalayi@gmail.com>
2026-01-21 23:26:17 +0530

    chore: add tailwind and types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,9 @@
-import { NavLink } from "react-router";
-
 export function Home() {
   return (
     <>
-      <h1>Home Page</h1>
-      <NavLink to="/events/1">
-        {({ isPending }) => (
-          <span>{isPending ? "loading..." : "Web Design"}</span>
-        )}
-      </NavLink >
-      <br />
-      <NavLink to="/events/2">
-        {({ isPending }) => (
-          <span>{isPending ? "loading..." : "Debugging"}</span>
-        )}
-      </NavLink >
+      <h1 className="text-3xl font-bold underline text-blue-900">
+        Hello world!
+      </h1>
     </>
   )
 }
