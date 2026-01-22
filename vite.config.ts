2f928306d2cc35ee5111988773addd364d3463ab
Faris P <farispalayi@gmail.com>
2026-01-22 21:27:48 +0530

    perf: js chunk splitting & bundle visualizing

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss(), mode === 'analyze' && visualizer({
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+  }) as PluginOption].filter(Boolean),
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'framer-motion': ['framer-motion'],
+          'vendor': ['react', 'react-dom']
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}))
