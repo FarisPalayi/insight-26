b7e64693878b4093120cda19db9606ccb6654c36
Faris P <farispalayi@gmail.com>
2026-01-29 03:02:16 +0530

    chore: add gsap scrollsmoother

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,19 +6,33 @@ import SponsorSection from "@/components/features/sponsor/SponsorSection";
 import Main from "@/components/layout/Main";
 import PastEditionHighlights from "@/components/features/pastHighlights/pastHighlightsSection";
 import FAQSection from "@/components/features/faq/FAQSection";
+import EventSchedule from "@/Schedule";
+import { useGSAP } from "@gsap/react";
+import { ScrollSmoother } from "gsap/all";
 
 export function Home() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+      normalizeScroll: true,
+    })
+  })
+
   return (
-    <div className="min-h-screen bg-background dark">
-      <Header />
-      <Main className="">
-        <HeroSection />
-        <EventsSection />
-        <PastEditionHighlights />
-        <SponsorSection />
-        <FAQSection />
-      </Main>
-      <Footer />
+    <div className="min-h-screen bg-background dark" id="smooth-wrapper">
+      <div id="smooth-content">
+        <Header />
+        <Main className="">
+          <HeroSection />
+          <EventsSection />
+          <PastEditionHighlights />
+          <EventSchedule />
+          <SponsorSection />
+          <FAQSection />
+        </Main>
+        <Footer />
+      </div>
     </div>
   )
 }
