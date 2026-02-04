657a8b028c4c0179602d40fe31990568a6fc148b
Faris P <farispalayi@gmail.com>
2026-02-05 00:06:23 +0530

    proto: add gsap scroll smoother

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,8 +2,18 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { Outlet, ScrollRestoration, useNavigation } from "react-router";
 import { EventsPageSkeleton } from "@/components/features/events/skeletons/EventPageSkeleton";
+import { useGSAP } from "@gsap/react";
+import { ScrollSmoother } from "gsap/all";
 
 export default function RootLayout() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+      normalizeScroll: true,
+    })
+  })
+
   const navigation = useNavigation();
   const isPending = navigation.state === "loading";
   const nextPath = navigation.location?.pathname;
@@ -12,13 +22,16 @@ export default function RootLayout() {
 
 
   return (
-    <div className="min-h-screen bg-background dark">
-      {/* to ensure that page starts from the top of a page */}
-      <ScrollRestoration />
-      <Header />
-      {showEventsSkeleton && <EventsPageSkeleton />}
-      <Outlet />
-      <Footer />
+    <div className="min-h-screen bg-background dark" id="smooth-wrapper">
+      <div id="smooth-content">
+
+        {/* to ensure that page starts from the top of a page */}
+        <ScrollRestoration />
+        <Header />
+        {showEventsSkeleton && <EventsPageSkeleton />}
+        <Outlet />
+        <Footer />
+      </div>
     </div>
   );
 }
