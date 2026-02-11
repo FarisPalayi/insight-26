bfdd6b32257fca589b39cb91b7bb5547250a9eb8
Faris P <farispalayi@gmail.com>
2026-02-12 02:19:31 +0530

    chore: update faq data

--- src/components/features/faq/faqData.ts
+++ src/components/features/faq/faqData.ts
@@ -2,9 +2,12 @@ import type { FAQ } from "@/types";
 
 export const faqs: FAQ[] = [
   {
-    question: "How do I register for Insight'26?",
+    question: "What are the general guidelines for participants of Insight'26?",
     answer:
-      "Register through our official website by filling out the registration form with your details. Spot registration is also available at the venue on event days.",
+      "Registration is mainly online; limited spot registration may be available.\
+       Participants must carry a valid college ID and report at least 30 minutes before the event.\
+       Any rule violation or misconduct leads to disqualification.\
+       Judges’ and committee decisions are final.",
   },
   {
     question: "Who can participate in Insight'26?",
@@ -22,9 +25,13 @@ export const faqs: FAQ[] = [
       "All participants receive participation certificates. Winners get cash prizes, trophies, and winner certificates. Prize amounts vary by event category.",
   },
   {
+    question: "Where will Insight'26 tech fest be held?",
+    answer: "Insight’26 will take place at the Calicut University Campus, Thenhipalam, Malappuram. You can find all individual event locations through the Venues link available in the navigation menu (hamburger menu on mobile)."
+  },
+  {
     question: "Can events be cancelled?",
     answer:
-      "Events require minimum participants to proceed. If an event doesn't meet the threshold, participants will be notified and can switch to other events.",
+      "Events require minimum participants to proceed. If an event doesn't meet the threshold, participants will be notified and registration fee will be refunded.",
   },
   {
     question: "Can I participate in multiple events?",
