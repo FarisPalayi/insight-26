9a7527347059d4833bc22830e39a719f4769ca96
Faris P <farispalayi@gmail.com>
2026-02-11 17:24:21 +0530

    chore: remove unused gsap dependencies

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -3,10 +3,8 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 // ScrollSmoother requires ScrollTrigger
 import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { SplitText } from "gsap/SplitText";
-import { TextPlugin } from "gsap/TextPlugin";
 
-gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, SplitText, TextPlugin);
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
 
-export { gsap, ScrollTrigger, ScrollSmoother, SplitText, TextPlugin };
+export { gsap, ScrollTrigger, ScrollSmoother };
 export { useGSAP } from '@gsap/react';
